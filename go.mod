d348aba035807069bc5f2583c2f0fdf98d4112be
Steven Hammon <steve@hammon.com>
2025-07-22 09:52:45 -0500

    added all HL7v2.3 types

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/s-hammon/faraday
 
 go 1.24.5
 
+require github.com/s-hammon/p v0.0.0-20250711025910-56625589421f
+
 require (
-	github.com/s-hammon/p v0.0.0-20250711025910-56625589421f // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- types.go
+++ types.go
@@ -7,20 +7,30 @@ import (
 )
 
 // these are the basic HL7 "types"
-// pretty much everything is a string
 
-// string
-// usually indicates "freeform" fields like patient names and procedures
+/*
+	ALPHANUMERIC
+*/
+
+// String
 type ST string
 
-// numeric
-// ....but actually, a string lol
+// Text
+type TX string
+
+// Formatted Text
+type FT string
+
+/*
+	NUMERICAL
+*/
+
+// Numeric
 type NM string
 
 // TODO: implement ToNumeric
 
-// sequence ID
-// supposed to be a non-negative integer type
+// Sequence ID (integer)
 type SI string
 
 func (s SI) ToNumeric() int {
@@ -31,27 +41,55 @@ func (s SI) ToNumeric() int {
 	return i
 }
 
-// Coded value from user-defined table
-// aka custom values
+// Composite quantity
+type CQ struct {
+	Quantity NM
+	Units    CE
+}
+
+// Money
+type MO struct {
+	Quantity     NM
+	Denomination ID
+}
+
+type comparator string
+
+const (
+	GT comparator = ">"
+	LT comparator = "<"
+	GE comparator = ">="
+	LE comparator = "<="
+	EQ comparator = "="
+	NE comparator = "<>"
+)
+
+// Structured Numeric
+type SN struct {
+	Comparator comparator
+	FirstNum   NM
+	SecondNum  NM
+	Separator  ST
+}
+
+func (sn *SN) IsValidType() bool {
+	switch comparator(sn.Comparator) {
+	default:
+		return false
+	case GT, LT, GE, LE, EQ, NE:
+		return true
+	}
+}
+
+/*
+	IDENTIFIER
+*/
+
+// Coded value for user-defined tables
 type IS string
 
-// Coded value from "official" table
-// these usually follow a convention but aren't actually enforced lol
+// Coded value for HL7 tables
 type ID string
-
-// Timestamp, in the string format
-type TS string
-
-// TODO: implement ToDatetime
-
-// Datetime, like TS
-type DT string
-
-// Telephone number
-// Yep, that gets its own type but not SSN
-type TN string
-
-// TODO: implement ToPhone(?)
 
 // Hierarchic Designator
 type HD struct {
@@ -64,28 +102,103 @@ func (hd *HD) IsValidType() bool {
 	return UniversalIdTypes.Valid(hd.UniversalIdType)
 }
 
-// Message Type
-type CM_MSG struct {
-	MessageType  ID
-	TriggerEvent ID
+// Entity Identifier
+type EI struct {
+	EntityIdentifier ST
+	NamespaceId      IS
+	UniversalId      ST
 }
 
-func (cm *CM_MSG) IsValidType() bool {
-	return UniversalIdTypes.Valid(cm.MessageType) &&
-		UniversalIdTypes.Valid(cm.TriggerEvent)
+// Reference Pointer
+type RP struct {
+	Pointer       ST
+	ApplicationId HD
+	DataType      ID
+	DataSubType   ID
+}
+
+// Person Location
+type PL struct {
+	PointOfCare         IS
+	Room                IS
+	Bed                 IS
+	Facility            HD
+	LocationStatus      IS
+	PersonLocationType  IS
+	Building            IS
+	Floor               IS
+	LocationDescription ST
 }
 
 // Processing Type
-// perfect example of the nonstandard standard of HL7--the fields are ST type
-// but reference a table :)
 type PT struct {
-	ProcessingId   ST
-	ProcessingMode ST
+	ProcessingId   ID
+	ProcessingMode ID
 }
 
-// Extended Composite ID w/ Check Digit
+/*
+	DATETIME
+*/
+
+// Date YYYY[MM[dd]]
+type DT string
+
+// Time HH[MM[SS[.S[S[S[S]]]]]][+/-ZZZZ]
+type TM string
+
+// Timestamp YYYY[MM[dd]]HH[MM[SS[.S[S[S[S]]]]]][+/-ZZZZ]
+type TS string
+
+// TODO: implement ToDatetime
+
+/*
+	CODE VALUES
+*/
+
+// Coded Element
+type CE struct {
+	Identifier            ST
+	Text                  ST
+	CodingSystem          ST
+	AlternateIdentifier   ID
+	AlternateText         FT
+	AlternateCodingSystem ST
+}
+
+// Coded Element with formatted values
+type CF struct {
+	Identifier            ID
+	FormattedText         FT
+	CodingSystem          ST
+	AlternateIdentifier   ID
+	AlternateText         FT
+	AlternateCodingSystem ST
+}
+
+// Composite ID with check digit
+type CK struct {
+	IdNumber             NM
+	CheckDigit           NM
+	CheckDigitSchemeCode ID
+	AssigningAuthority   HD
+}
+
+// Composite ID number and name
+type CN struct {
+	IdNumber           ST
+	FamilyName         ST
+	GivenName          ST
+	MiddleName         ST
+	Suffix             ST
+	Prefix             ST
+	Degree             ST
+	SourceTable        IS
+	AssigningAuthority HD
+}
+
+// Extended Composite ID with check digit
 type CX struct {
-	Id                   ST
+	IdNumber             ST
 	CheckDigit           ST
 	CheckDigitSchemeCode ID
 	AssigningAuthority   HD
@@ -99,20 +212,57 @@ func (cx *CX) IsValidType() bool {
 		IdentifierTypeCodes.Valid(cx.CheckDigitSchemeCode)
 }
 
-// Extended Person Name
-type XPN struct {
-	FamilyName             ST
-	GivenName              ST
-	MiddleName             ST
-	Suffix                 ST
-	Prefix                 ST
-	Degree                 ST
-	NameTypeCode           ID
-	NameRepresentationCode ID
+// Extended Composite ID number and name
+type XCN struct {
+	IdNumber             ST
+	FamilyName           ST
+	GivenName            ST
+	MiddleName           ST
+	Suffix               ST
+	Prefix               ST
+	Degree               ST
+	SourceTable          IS
+	AssigningAuthority   HD
+	NameTypeCode         ID
+	IdentifierCheckDigit ST
+	CheckDigitSchemeCode ID
+	IdentifierTypeCode   IS
+	AssigningFacility    HD
 }
 
+// Composite -- apparently they aren't allowed
+type CM struct{}
+
+/*
+	DEMOGRAPHICS
+*/
+
+// Address
+type AD struct {
+	StreetAddress              ST
+	OtherDesignation           ST
+	City                       ST
+	State                      ST
+	Zip                        ST
+	Country                    ID
+	AddressType                ID
+	OtherGeographicDesignation ST
+}
+
+// Person Name
+type PN struct {
+	FamilyName ST
+	GivenName  ST
+	MiddleName ST
+	Suffix     ST
+	Prefix     ST
+	Degree     ST
+}
+
+// Telephone number [NN][(999)]999-9999[X99999][B99999]
+type TN string
+
 // Extended Address
-// last 2 fields are IS, but have no table--LOL
 type XAD struct {
 	StreetAddress              ST
 	OtherDesignation           ST
@@ -126,32 +276,160 @@ type XAD struct {
 	CensusTract                IS
 }
 
+// Extended Person Name
+type XPN struct {
+	FamilyName   ST
+	GivenName    ST
+	MiddleName   ST
+	Suffix       ST
+	Prefix       ST
+	Degree       ST
+	NameTypeCode ID
+}
+
+// Extended Composite Name and ID number for organizations
+type XON struct {
+	OrganizationName     ST
+	TypeCode             IS
+	IdNumber             NM
+	CheckDigit           NM
+	CheckDigitSchemeCode ID
+	AssigningAuthority   HD
+	IdentifierTypeCode   IS
+	AssigningFacility    HD
+}
+
 // Extended Telecommunication Number
 type XTN struct {
-	TelephoneNumber                TN
-	TelecommunicationUseCode       ID
-	TelecommunicationEquipmentType ID
-	EmailAddress                   ST
-	CountryCode                    NM
-	AreaCode                       NM
-	PhoneNumber                    NM
-	Extension                      NM
-	AAnyText                       ST
+	TelecommunicationUseCode ID
+	EquipmentType            ID
+	EmailAddress             ST
+	CountryCode              NM
+	AreaCode                 NM
+	PhoneNumber              NM
+	Extension                NM
+	AnyText                  ST
 }
 
-// Coded Element -- everyone's favorite type because it's very, very liberal
-type CE struct {
-	Identifier            ST
-	Text                  ST
-	CodingSystem          ST
-	AlternateIdentifier   ST
-	AlternateText         ST
-	AlternateCodingSystem ST
+/*
+	EXTENDED QUERIES
+*/
+
+// Query Selection Criteria
+type QSC struct {
+	FieldName             ST
+	RelationalOperator    ID
+	Value                 ST
+	RelationalConjunction ID
 }
 
-// Driver's License Number -- but again, SSN is just a plain ol' string
+// Query Input Parameter
+type QIP struct {
+	FieldName ST
+	Values    []ST // TODO: check this
+}
+
+// Row Column Definition
+type RCD struct {
+	Hl7ItemNumber      ST
+	Hl7DataType        ST
+	MaximumColumnWidth NM
+}
+
+/*
+	MASTER FILES
+*/
+
+// Driver's License Number
 type DLN struct {
 	LicenseNumber  ST
 	IssuingState   IS
 	ExpirationDate DT
+}
+
+// Job Code/Class
+type JCC struct {
+	JobCode  IS
+	JobClass IS
+}
+
+// Visiting hours
+type VH struct {
+	StartDayRange  ID
+	EndDayRange    ID
+	StartHourRange TM
+	EndHourRange   TM
+}
+
+/*
+	MEDICAL RECORDS/INFORMATION MANAGEMENT
+*/
+
+// Performing Person time stamp
+type PPN struct {
+	FamilyName              ST
+	GivenName               ST
+	MiddleName              ST
+	Suffix                  ST
+	Prefix                  ST
+	Degree                  ST
+	SourceTable             IS
+	AssigningAuthority      HD
+	NameTypeCode            ID
+	IdentifierCheckDigit    ST
+	CheckDigitSchemeCode    ID
+	IdentifierTypeCode      IS
+	AssigningFacility       HD
+	DateTimeActionPerformed TS
+}
+
+/*
+	TIME SERIES
+*/
+
+// Date/Time Range
+type DR struct {
+	StartDateTime TS
+	EndDateTime   TS
+}
+
+// Repeat Interval
+type RI struct {
+	RepeatPattern IS
+	TimeInterval  ST
+}
+
+// Scheduling Class Value Pair
+type SCV struct {
+	ParameterClass IS
+	ParameterValue IS
+}
+
+type interval string
+type duration string
+type sequencing string
+
+// Timing/quantity
+type TQ struct {
+	Quantity           CQ
+	Interval           interval
+	Duration           duration
+	StartDateTime      TS
+	EndDateTime        TS
+	Priority           ID
+	Condition          ST
+	Text               TX
+	Conjunction        ID
+	OrderingSequencing sequencing
+}
+
+// Message Type
+type CM_MSG struct {
+	MessageType  ID
+	TriggerEvent ID
+}
+
+func (cm *CM_MSG) IsValidType() bool {
+	return UniversalIdTypes.Valid(cm.MessageType) &&
+		UniversalIdTypes.Valid(cm.TriggerEvent)
 }
